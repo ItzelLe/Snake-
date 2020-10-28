2746509c50257b9dc880ad542c3e6a927b77fb37
ItzelLe <73494752+ItzelLe@users.noreply.github.com>
2020-10-28 11:55:00 -0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,25 +1,35 @@
 from turtle import *
 from random import randrange, choice
 import random
-from freegames import square, vector
+from freegames import square, vector 
+
+color1 = random.randint(0,4)
+color2 = random.randint(0,4)
+
+while color2 == color1:
+    color2 = random.randint(0, 4)
+
+color3 = ['blue', 'purple', 'pink', 'green', 'black']
+color4 = ['blue', 'purple', 'pink', 'green', 'black']
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+
 
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+
 def inside(head):
     "Return True if head inside boundaries."
-    return -200 < head.x < 190 and -200 < head.y < 190
+    return -200 < head.x < 190 and -200 < head.y < 190 
+
 
 def move():
     "Move snake forward one segment."
-    head = snake[-1].copy() 
-    head.move(aim) 
     head = snake[-1].copy() 
     head.move(aim) 
     food.move(vector(choice([-10, 0, 10]), choice([-10, 0, 10])))
@@ -39,18 +49,18 @@ def move():
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
-        snake.pop(0)
+        snake.pop(0) 
 
     clear()
-
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, color3[color1])
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, color4[color2])
     update()
-    ontimer(move, 100)
+    ontimer(move, 75) 
 
-setup(420, 420, 370, 0)
+
+setup(420, 420, 370, 0) 
 hideturtle()
 tracer(False)
 listen()
